b23daebf4ce71d959b6e491815ef9a1a75b030ad
Naman Agarwal <namana019@outlook.com>
2020-11-02 15:13:13 +0530

    Initial Commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	log.Println("Server is running on port 8000")
+
+	route := mux.NewRouter()
+
+	log.Fatal(http.ListenAndServe(":8000", route))
+}
